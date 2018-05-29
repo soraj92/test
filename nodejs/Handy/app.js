5e6f0125d3c44d4c76187fe0c8d0bddff4081ab4
전소라 <soraj92@gmail.com>
2018-05-29 14:25:43 +0900

    업로드

---
+++ nodejs/Handy/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const static = require('serve-static');
+const app = express();
+const router = express.Router();
+const config = require('./config');
+
+app.set('port', process.env.PORT || config.server_port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+router.route('/logistics/hlc').post(function(req,res) {
+
+    console.log('들어옴');
+    console.dir(req);
+
+    res.write('1');
+    res.end();
+    // const a = req.body;
+    // console.dir(a);
+    // const b = Object.getOwnPropertyNames(a);
+
+    // const c = b[0].split('\n');
+    // for(var i = 0; i <c.length; i++)
+    //     console.log(c[i]);
+
+});
+
+router.route('/logistics/hlc').get(function(req,res) {
+
+    console.log('들어옴');
+    console.dir(req);
+    res.write('1');
+    res.end();
+
+    // const a = req.body;
+    // console.dir(a);
+    // const b = Object.getOwnPropertyNames(a);
+
+    // const c = b[0].split('\n');
+    // for(var i = 0; i <c.length; i++)
+    //     console.log(c[i]);
+
+});
+
+app.use('/',router);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('서버 실행 : ' + app.get('port'));
+});
---
+++ nodejs/Handy/config.js
@@ -0,0 +1,5 @@
+const config = {
+    server_port : 3010
+};
+
+module.exports = config;
