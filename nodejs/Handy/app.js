1ff2a48fb7b7eec8d50cdd9bf88c91d269651ea6
soraj92 <soraj92@gmail.com>
2018-06-05 09:53:25 +0900

    mariadb 연동, 외부접속 확인

--- nodejs/Handy/app.js
+++ nodejs/Handy/app.js
@@ -12,7 +12,7 @@ const route_loader = require('./route/route_loder');
 function Connection() {
     conn.connect({
         host: "localhost",
-        port: 3307,
+        port: 3306,
         user: "root",
         password: '12345',
         database:"data",
