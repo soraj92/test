02185e59670655662519732e9b6f442d2cbf0b9e
전소라 <soraj92@gmail.com>
2018-06-19 14:55:23 +0900

    동기 테스트

--- nodejs/Handy/route/handy.js
+++ nodejs/Handy/route/handy.js
@@ -1,12 +1,16 @@
+const async = require('async');
+
 const logistics = function (req, res) {
 
     let body = [];
     
-    req.on('data', function(chunk) {
-        body.push(chunk);
-    }).on('end', function() {
-        console.log('end');
-    });
+    async.waterfall([function() {
+        req.on('data', function(chunk) {
+            body.push(chunk);
+        }).on('end', function() {
+            console.log('end');
+        });
+    }]);
 
     console.log('이벤트end보다 후');
 }
