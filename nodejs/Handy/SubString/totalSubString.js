bb448581b5a9d48aee687b449d7b1f88819a4ed9
전소라 <soraj92@gmail.com>
2018-06-21 13:09:05 +0900

    test

--- nodejs/Handy/SubString/totalSubString.js
+++ nodejs/Handy/SubString/totalSubString.js
@@ -19,31 +19,63 @@ function totalsubString(info, total_binds) {
         if (!info[i]) {
             continue;
         }
-       const codecheck = info[i].substring(0, 2).trim();
+        const codecheck = info[i].substring(0, 2).trim();
 
         if (codecheck == 05) {
             sub = require('./sub05');
-            sub.init(info[i], total_binds[0], function() {
+            sub.init(info[i], total_binds[0], function () {
+
+            });
+        } else if (codecheck == 10) {
+
+            sub = require('./sub10');
+            sub.init(info[i], total_binds[1], function () {
+
+            });
+        } else if (codecheck == 11 || codecheck == 31 || codecheck == 42) {
+
+            sub = require('./sub113142');
+            sub.init(info[i], total_binds[2], function () {
+
+            });
+        } else if (codecheck == 20 || codecheck == 21) {
+
+            sub = require('./sub2021');
+            sub.init(info[i], total_binds[3], function () {
+
+            });
+        } else if (codecheck == 23) {
+
+            sub = require('./sub23');
+            sub.init(info[i], total_binds[4], function () {
+
+            });
+        } else if (codecheck == 24 || codecheck == 25) {
+            sub = require('./sub2425');
+            sub.init(info[i], total_binds[5], function () {
+
+            });
+        } else if (codecheck == 32) {
+
+            sub = require('./sub32');
+            sub.init(info[i], total_binds[6], function () {
+
+            });
+        } else if (codecheck == 40) {
+            sub = require('./sub40');
+            sub.init(info[i], total_binds[7], function () {
+
+            });
+        } else if (codecheck == 41) {
+            sub = require('./sub41');
+            sub.init(info[i], total_binds[8], function () {
 
             });
         }
-        else if (codecheck == 10)
-            sub = require('./sub10');
-        else if (codecheck == 11 || codecheck == 31 || codecheck == 42)
-            sub = require('./sub113142');
-        else if (codecheck == 20 || codecheck == 21)
-            sub = require('./sub2021');
-        else if (codecheck == 23)
-            sub = require('./sub23');
-        else if (codecheck == 24 || codecheck == 25)
-            sub = require('./sub2425');
-        else if (codecheck == 32)
-            sub = require('./sub32');
-        else if (codecheck == 40)
-            sub = require('./sub40');
-        else if (codecheck == 41)
-            sub = require('./sub41');
+
     }
+
+    
 }
 
 module.exports = info;
