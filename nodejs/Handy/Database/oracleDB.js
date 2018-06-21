c60caf4650300db58ab56abac20fa1f6b7cfc186
전소라 <soraj92@gmail.com>
2018-06-21 16:22:09 +0900

    test

--- nodejs/Handy/Database/oracleDB.js
+++ nodejs/Handy/Database/oracleDB.js
@@ -13,15 +13,18 @@ oracle.init = function (total_binds, callback) {
             console.error(err.message);
             return;
         }
+
         let start;
-        for (var i = 0; i < total_binds.length; i++)
+        for (var i = 0; i < total_binds.length; i++) {
             if (i == 0)
                 start = require('./sub05');
             else if (i == 3)
-            start = require('./sub2021');
-        if (total_binds[i].length != 0) {
+                start = require('./sub2021');
+                
+            if (total_binds[i].length != 0) {
 
-            start.init(oracledb, connection, total_binds[i]);
+                start.init(oracledb, connection, total_binds[i]);
+            }
         }
     });
 
