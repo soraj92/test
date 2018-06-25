499f50c964e4dd7506b707c0a11a7a758fe03066
전소라 <soraj92@gmail.com>
2018-06-25 15:27:45 +0900

    test

--- nodejs/Handy/SubString/totalSubString.js
+++ nodejs/Handy/SubString/totalSubString.js
@@ -82,7 +82,7 @@ function totalsubString(info, total_binds, callback) {
     }
     const log = require('../common/total-logger');
     const logger = new log(collection);
-   logger.logger.info(info);
+    logger.logger.info(info);
     const log2 = require('../common/part-logger');
     const logger2 = new log2(collection);
     logger2.logger.info(info);
