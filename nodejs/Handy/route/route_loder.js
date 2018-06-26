ac28b5f6951e51a6f049b30d0df533b250f6c9eb
전소라 <soraj92@gmail.com>
2018-06-26 10:19:18 +0900

    test

--- nodejs/Handy/route/route_loder.js
+++ nodejs/Handy/route/route_loder.js
@@ -15,12 +15,11 @@ function initRoutes(app, router) {
     for (var i = 0; i < infoLen; i++) {
         const curItem = config.route_info[i];
         const curModule = require(curItem.file);
-        const module_name = curItem.method;
-        console.log(module_name)
+
         if (curItem.type == 'post') {
-            router.route(curItem.path).post(curModule[module_name]);
+            router.route(curItem.path).post(curModule[curItem.method]);
         } else if (curItem.type == 'get') {
-            router.route(curItem.path).get(curModule[module_name]);
+            router.route(curItem.path).get(curModule[curItem.method]);
         } else
             console.log('post나 get이 아님');
     }
